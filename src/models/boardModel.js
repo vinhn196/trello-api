dcec2a1a260342b2e8e6e2c224ee2b23383e24f3
vinhn196 <vinhn96hn@gmail.com>
2024-03-20 16:13:27 +0700

    create_model

---
+++ src/models/boardModel.js
@@ -0,0 +1,18 @@
+import Joi from 'joi'
+
+const BOARD_COLLECTION_NAME = 'boards'
+const BOARD_COLLECTION_SCHEMA = Joi.object({
+  title: Joi.string().required().min(3).max(50).trim().strict(),
+  slug: Joi.string().required().min(3).trim().strict(),
+  description: Joi.string().required().min(3).max(256).trim().strict(),
+
+  columnOrderIds: Joi.array().items(Joi.string()).default([]),
+  createdAt: Joi.date().timestamp('javascript').default(Date.now),
+  updateAt: Joi.date().timestamp('javascript').default(Date.now),
+  _destroy: Joi.boolean().default(false)
+})
+
+export const boardModel = {
+  BOARD_COLLECTION_NAME,
+  BOARD_COLLECTION_SCHEMA
+}
