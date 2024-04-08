864205c960907e3a21d71eeb2545c22db86a89f2
vinhn196 <vinhn96hn@gmail.com>
2024-04-08 15:13:02 +0700

    deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const WHITELIST_DOMAINS = [
   // Không cần localhost nữa vì file config/cors đã luôn luôn cho phép môi trường dev
   // 'http://localhost:5173'
-  'https://trello-web-lac.vercel.app'
+  'https://trello-web-vinhnguyendev.vercel.app'
 ]
 
 export const BOARD_TYPES = {
